d3512fbfcfe86a41f91976cf2fe5786933ed7f45
ekhurtado <e16hurtado@gmail.com>
2023-05-14 13:34:06 +0200

    NodeRED nodo pertsonalizatuen bertsio funtzionala bukatuta

--- Kodea/IDE_plataforma/nodoak/sink/showNumbers/show_numbers.js
+++ Kodea/IDE_plataforma/nodoak/sink/showNumbers/show_numbers.js
@@ -2,7 +2,6 @@ const xml2js = require('xml2js');
 const builder = new xml2js.Builder();
 const fs = require('fs');
 
-
 // Aplikazio-eredua osatzeko elementu erabilgarrien liburutegia inportatu
 const {FunctionInfo, createLastMicroservice} = require('../appModel_utils');
 
@@ -10,24 +9,13 @@ const {FunctionInfo, createLastMicroservice} = require('../appModel_utils');
 const componentName = "ShowNumbers";
 const codePath = "gcr.io/gcis/show-numbers:latest";
 
-// TODO BORRAR
-var appmodelAnterior = "<Application name=\"NumbersProcessing\">\n" +
-    "\t\t<Microservice name=\"CreatingNumber\" service=\"NaturalValue\"\n" +
-    "\t\t\t\t\t\tcustomization=\"{type: 'random'}\">\n" +
-    "\t\t\t<outPort name=\"CreatingNumberOPort\" \n" +
-    "\t\t\t\tprotocol=\"HTTP\" dataType=\"TNumber\"/>\n" +
-    "\t\t</Microservice>\t\n" +
-    "\n" +
-    "\t\t\n" +
-    "\t\t<channel from=\"CreatingNumbersOPort\"/>" +
-    "</Application>\t\n"
-
 module.exports = function(RED) {
     function ShowNumbers(config) {
         RED.nodes.createNode(this,config);
         
         this.function = config.function;
         this.selectedPortNumber = config.portnumber;
+        this.selectedFileName = config.filename;
         var node = this;
         
         node.on('input', function(msg) {
@@ -48,16 +36,26 @@ module.exports = function(RED) {
                 // Hautatutako funtzionalitatearen informazioa lortzen dugu
                 // --------------------
                 let selectedFunctionInfo;
+                let selectedCustomizationValue = "";
                 for (const funcObj in allFunctionsInfo) {
-                    if (node.function === allFunctionsInfo[funcObj].name)
+                    if (node.function === allFunctionsInfo[funcObj].name) {
                         selectedFunctionInfo = allFunctionsInfo[funcObj];
+                        if (node.function === "SaveTXT" || node.function === "SaveCSV") {
+                            if (node.selectedFileName === "") {
+                                node.error("Ez duzu fitxategi izenik zehaztu. Mesedez, zehaztu ezazu.");
+                                return;
+                            } else
+                                selectedCustomizationValue = node.selectedFileName;
+                        }
+                    }
                 }
 
                 // Mikrozerbitzu berriaren informazioa eraikitzen dugu
                 // --------------------
                 const newMicroservice = createLastMicroservice(componentName, codePath, selectedFunctionInfo, node.selectedPortNumber);
                 // Osagai honen pertsonalizazioa gehitzen diogu (osagai honen bereizgarria dena)
-                //newMicroservice.$.customization = `{${selectedFunctionInfo.customizationName}: ${selectedCustomizationValue}}`;
+                if (selectedCustomizationValue !== "")
+                    newMicroservice.$.customization = `{${selectedFunctionInfo.customizationName}: '${selectedCustomizationValue}'}`;
 
 
                 // Aurreko osagaiak bidalitako aplikazio-eredua lortzen dugu
