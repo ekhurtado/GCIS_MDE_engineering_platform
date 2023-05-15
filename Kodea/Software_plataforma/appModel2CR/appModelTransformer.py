351bad648437b71ae3b817727c2a46338b7e3d01
Ekaitz HURTADO <839073@adm.ehu.es>
2023-05-15 13:43:35 +0200

    XML aplikazio-eredutik Kuberneteseko CustomResource formatura bihurtzeko programa bukatuta

--- Kodea/Software_plataforma/appModel2CR/appModelTransformer.py
+++ Kodea/Software_plataforma/appModel2CR/appModelTransformer.py
@@ -1,9 +1,15 @@
+import json
+import os
+
 # XML fitxategia irakurtzeko liburutegia
 import xml.etree.ElementTree as ET
 
 # Fitxategiak aukeratzeko liburutegia
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+
+# YAML fitxategiekin lan egiteko liburutegia
+import yaml
 
 
 def getAppModel():
@@ -18,9 +24,12 @@ def getAppModel():
             print("Sartutako aukera ez da zuzena, sar ezazu berriro, mesedez.")
     print(selectedOption)
     if selectedOption == 1:
-        Tk()
+        window = Tk()
+        window.lift()
+        window.attributes("-topmost", True)  # Leihoa pantailan erakusteko
+        window.after_idle(window.attributes, '-topmost', False)
         Tk().withdraw()
-        archivo_xml = askopenfilename(filetypes=[("Archivos XML", "*.xml")])
+        archivo_xml = askopenfilename(filetypes=[("Archivos XML", "*.xml")], title="Aukeratu fitxategia")
         return ET.parse(archivo_xml)
     else:
         print("Aplikazio-eredua kopia eta hemen itsas ezazu (amaitu Enter sakatuz lerro huts batean):")
@@ -42,19 +51,67 @@ def getAppModel():
         return stringAppModelObj
 
 
+def transformXMLToYAML(appModelXML):
+    appName = appModelXML.getroot().attrib.get("name")
+    print("APP NAME: " + appName)
+    appModelYAML = {
+        'apiVersion': 'ehu.gcis.org/v1alpha1',
+        'kind': 'Application',
+        'metadata': {
+            'name': appName
+        },
+        'spec': {
+            'components': [],
+            'channels': []
+        }
+    }
+
+    for microsvc in appModelXML.iter("Microservice"):
+        # Osagaiaren informazio lortuko dugu
+        componentInfo = {}
+        for key, value in microsvc.attrib.items():
+            if value.__contains__("'"):
+                value = value.replace("'", '"')
+            componentInfo[key] = value
+
+        # Sarrera edo/eta irteera portuen informazioa osagaian sartuko dugu
+        for port in microsvc:
+            componentInfo[port.tag] = {}
+            for key, value in port.attrib.items():
+                componentInfo[port.tag][key] = value
+
+        # Osagaiaren informazioa aplikazioan sartzen dugu
+        appModelYAML['spec']['components'].append(componentInfo)
+
+    for channel in appModelXML.iter("channel"):
+        # Kanalaren informazio lortuko dugu
+        channelInfo = {}
+        for key, value in channel.attrib.items():
+            channelInfo[key] = value
+        # Kanalaren informazioa aplikazioan sartzen dugu
+        appModelYAML['spec']['channels'].append(channelInfo)
+
+    return appModelYAML
+
+
 def main():
     print("Kaixo! Ongi etorri aplikazio-eredua Kuberneteseko Custom Resource elementuan transformatzeko kodera.")
-    appModelObject = getAppModel()
-    if appModelObject is not None:
-        appName = appModelObject.getroot().attrib.get("name")
-        print("APP NAME: " + appName)
-        for app in appModelObject.iter("Microservice"):
-            print(f"Etiqueta: {app.tag}")
-            print(f"Texto: {app.attrib}")
-        # for element in appModelObject.iter():
-        #     print(f"Etiqueta: {element.tag}")
-        #     print(f"Texto: {element.text}")
-        #     print(f"Texto: {element.attrib}")
+    appModelXML = getAppModel()
+    if appModelXML is not None:
+        # XML aplikazio-eredutik Custom Resource den YAML aplikazio-eredua lortuko dugu
+        appModelYaml = transformXMLToYAML(appModelXML)
+        # Aplikazio-eredua YAML formatuan edukita, fitxategian gordeko dugu
+        appName = appModelXML.getroot().attrib.get("name")
+        f = open(appName + '_CR.yaml', 'w')
+        yaml.dump(appModelYaml, f)
+        print("Aplikazio-eredua sortuta. Fitxategia programa honen karpeta berdinean aurki dezakezu:")
+        print("\t" + os.path.realpath(f.name))
+        f.close()
 
 
+string = '{"type": "random"}'
+# decoded_string = bytes(string, "utf-8").decode("unicode_escape")
+# string = string.replace("''", '"')
+print(string)
+aa = json.loads(string)
 main()
