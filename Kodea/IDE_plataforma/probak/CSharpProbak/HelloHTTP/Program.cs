3d92c9a90db5fb3c4955df7a63b4a7a5e722f3c3
ekhurtado <e16hurtado@gmail.com>
2023-05-24 20:33:17 +0200

    ZenbakienSorkuntza osagaiaren kodea aurreratuta

--- Kodea/IDE_plataforma/probak/CSharpProbak/HelloHTTP/Program.cs
+++ Kodea/IDE_plataforma/probak/CSharpProbak/HelloHTTP/Program.cs
@@ -1,79 +1,87 @@
-﻿using System.Net.Http;
-using System.Text;
+﻿using System;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Random random = new Random();
-for (int i = 0; i < 5; i++)
+public class HelloHTTP
 {
-    int randomNumber = random.Next(1, 1001); // Generates a random number between 1 and 1000
-    Console.WriteLine($"Random Number {i + 1}: {randomNumber}");
-}
-
-// Send a GET request
-using (HttpClient client = new HttpClient())
-{
-string url = "http://localhost:5000"; // Replace with the desired URL
-HttpResponseMessage response = await client.GetAsync(url);
-
-if (response.IsSuccessStatusCode)
-{
-    string responseContent = await response.Content.ReadAsStringAsync();
-    Console.WriteLine("GET request successful");
-    Console.WriteLine("Response Content: " + responseContent);
-}
-else
-{
-    Console.WriteLine("GET request failed with status code: " + response.StatusCode);
-}
-}
-
-// Send a POST request with data
-using (HttpClient client = new HttpClient())
-{
-    string url = "http://localhost:5000"; // Replace with the desired URL
-
-    var postData = new Dictionary<string, string>
+    public static async Task Main(string[] args)
     {
-        { "param1", "25" },
-        { "param2", "67" }
-    };
+        Console.WriteLine("Hello, World!");
 
-    var content = new FormUrlEncodedContent(postData);
-    HttpResponseMessage response = await client.PostAsync(url, content);
+        string url = "http://localhost:5000"; // Replace with the desired URL
 
-    if (response.IsSuccessStatusCode)
-    {
-        string responseContent = await response.Content.ReadAsStringAsync();
-        Console.WriteLine("POST request successful");
-        Console.WriteLine("Response Content: " + responseContent);
-    }
-    else
-    {
-        Console.WriteLine("POST request failed with status code: " + response.StatusCode);
+        // Send a GET request
+        await HttpHelper.SendGetRequest(url);
+
+        // Send a POST request
+        string requestBody = "This is a simple string as the request body.";
+        await HttpHelper.SendPostRequest(url, requestBody);
+
     }
 }
 
-using (HttpClient client = new HttpClient())
-{
-    string requestBody = "Ahora un simple string";
-    Console.WriteLine(requestBody);
-    string url = "http://localhost:5000"; // Replace with the desired URL
+//using System.Net.Http;
+//using System.Text;
+//
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//
+//Random random = new Random();
+//for (int i = 0; i < 5; i++)
+//{
+//    int randomNumber = random.Next(1, 1001); // Generates a random number between 1 and 1000
+//    Console.WriteLine($"Random Number {i + 1}: {randomNumber}");
+//}
+//
+//string url = "http://localhost:5000"; // Replace with the desired URL
+//
+//// Send a GET request
+//await HttpHelper.SendGetRequest(url);
+//
+//// Send a POST request
+//string requestBody = "This is a simple string as the request body.";
+//await HttpHelper.SendPostRequest(url, requestBody);
 
-    var content = new StringContent(requestBody, Encoding.UTF8, "text/plain");
+// ---------------------------------------------------------
 
-    // Send the POST request
-    HttpResponseMessage response = await client.PostAsync(url, content);
-
-    if (response.IsSuccessStatusCode)
-    {
-        string responseContent = await response.Content.ReadAsStringAsync();
-        Console.WriteLine("POST request successful");
-        Console.WriteLine("Response Content: " + responseContent);
-    }
-    else
-    {
-        Console.WriteLine("POST request failed with status code: " + response.StatusCode);
-    }
-}
+//static async Task SendGetRequest()
+//{
+//    string url = "http://localhost:5000"; // Replace with the desired URL
+//
+//    using (HttpClient client = new HttpClient())
+//    {
+//        HttpResponseMessage response = await client.GetAsync(url);
+//
+//        if (response.IsSuccessStatusCode)
+//        {
+//            string responseContent = await response.Content.ReadAsStringAsync();
+//            Console.WriteLine("GET request successful");
+//            Console.WriteLine("Response Content: " + responseContent);
+//        }
+//        else
+//        {
+//            Console.WriteLine("GET request failed with status code: " + response.StatusCode);
+//        }
+//    }
+//}
+//
+//static async Task SendPostRequest(string requestBody)
+//{
+//    string url = "http://localhost:5000"; // Replace with the desired URL
+//
+//    using (HttpClient client = new HttpClient())
+//    {
+//        string requestBody = "Ahora un simple string";
+//        var content = new StringContent(requestBody, Encoding.UTF8, "text/plain");
+//        HttpResponseMessage response = await client.PostAsync(url, content);
+//
+//        if (response.IsSuccessStatusCode)
+//        {
+//            string responseContent = await response.Content.ReadAsStringAsync();
+//            Console.WriteLine("POST request successful");
+//            Console.WriteLine("Response Content: " + responseContent);
+//        }
+//        else
+//        {
+//            Console.WriteLine("POST request failed with status code: " + response.StatusCode);
+//        }
+//    }
+//}
---
+++ Kodea/Osagaiak/ZenbakienSorkuntza/ZenbakienSorkuntza/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+public class ZenbakienSorkuntza
+{
+
+//    string function = Environment.GetEnvironmentVariable("SERVICE");
+//    string customization = Environment.GetEnvironmentVariable("CUSTOMIZATION");
+    static string url = "http://localhost:5000"; // Replace with the desired URL
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Kaixo! Ongi etorri ZenbakienSorkuntza aplikaziora");
+
+        // Send a GET request
+        await Utils.SendGetRequest(url);
+
+        // Send a POST request
+        string requestBody = "This is a simple string as the request body.";
+        await Utils.SendPostRequest(url, requestBody);
+
+        // TODO EZABATU (INGURUNE-ALDAGAI BEZALA HARTU BEHAR DIRA)
+        string function = "NaturalValue";
+
+        Console.WriteLine(function);
+
+        switch (function)
+        {
+            case "NaturalValue":
+                await NaturalValue();
+                break;
+            case "IntegerValue":
+                await IntegerValue();
+                break;
+            case "FloatValue":
+                await FloatValue();
+                break;
+            default:
+                Console.WriteLine("Invalid function name.");
+                break;
+        }
+    }
+
+    public static async Task NaturalValue()
+    {
+        Console.WriteLine("NaturalValue funtzioa aukeratuta");
+        // TODO EZABATU (INGURUNE-ALDAGAI BEZALA HARTU BEHAR DIRA)
+        string customization = @"{""type"": ""decreasingValue"", ""firstvalue"": 3}";
+
+        // Lehenengo aldian hasierako zenbakia bidaliko da
+        (string type, int firstValue) = Utils.getCustomizationValues(customization);
+        string requestBody = @"{""type"": ""natural"",""value"": " +firstValue+ "}";
+        await Utils.SendPostRequest(url, requestBody);  // Send a POST request
+        Console.WriteLine(requestBody);
+        Thread.Sleep(5000); // Pause for 5 seconds
+
+        int createdValue = firstValue;
+        while (true)
+        {
+            createdValue = Utils.getNaturalValue(customization, createdValue);
+            Console.WriteLine("Created value: " + createdValue);
+
+            // Sortutako zenbakia bidaltzen dugu
+            requestBody = @"{""type"": ""natural"",""value"": " +createdValue+ "}";
+            await Utils.SendPostRequest(url, requestBody);  // Send a POST request
+
+            // Pause for 5 seconds
+            Thread.Sleep(5000);
+        }
+
+    }
+
+    public static async Task IntegerValue()
+    {
+        Console.WriteLine("IntegerValue funtzioa aukeratuta");
+    }
+
+    public static async Task FloatValue()
+    {
+        Console.WriteLine("FloatValue funtzioa aukeratuta");
+    }
+}
