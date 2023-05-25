1ab2a3dff1110307e2d3febbd7bfdeaff0958ceb
ekhurtado <e16hurtado@gmail.com>
2023-05-25 18:33:02 +0200

    ZenbakieProzesamendua osagaiaren kodearen lehenengo bertsio funtzionala

--- Kodea/Osagaiak/ZenbakienProzesamendua/zenbakienProzesamendua.py
+++ Kodea/Osagaiak/ZenbakienProzesamendua/zenbakienProzesamendua.py
@@ -1,9 +1,13 @@
+import json
 import os, requests
+from threading import Thread
+
 from flask import Flask, request
 
 function = os.environ.get('FUNCTION')
 inPortNumber = os.environ.get('INPORT_NUMBER')
 customization = os.environ.get('CUSTOMIZATION')
+# step = json.loads(customization)['urratsa']
 
 # TODO ezabatu
 inPortNumber = 7000
@@ -15,34 +19,45 @@ app = Flask(__name__)
 def main():
     print(request)
 
-    # TODO MODIFIKATU
+    # Lehenik eta behin, HTTP mezutik datuak lortuko ditugu
+    messageData = None
     if request.method == 'GET':
         # Handle GET requests
-        return "Hello, GET request!"
+        return "GET metodo bat bidali duzu!"
     elif request.method == 'POST':
         # Handle POST requests
-        print(request.data)
-        for key, value in request.form.items():
-            print("Key: {}, Value: {}".format(key, value))
-        print("Full HTTP Message:")
-        print("POST Request:")
-        print("Headers: ", request.headers)
-        print("Body: ", request.get_data(as_text=True))
-        return "Hello, POST request"
-    # ----------------------
+        if "text/plain" in request.headers.get("Content-Type"):
+            messageData = request.get_data(as_text=True)
+        # for key, value in request.form.items():
+        #     print("Key: {}, Value: {}".format(key, value))
+        # print("Full HTTP Message:")
+        # print("POST Request:")
+        # print("Headers: ", request.headers)
+        # print("Body: ", request.get_data(as_text=True))
+        # return "Hello, POST request"
+
+    print(messageData)
 
     # TODO ezabatu
     function = "BalioaHanditu"
 
+    thread_func = None  # Exekuzio-hari berria erabiltzen dugu aurreko osagaiari "OK" mezua lehen baino lehen bidaltzeko
     match function:
         case "BalioaHanditu":
-            increaseValue()
+            thread_func = Thread(target=increaseValue, args=(messageData,))
+            # increaseValue(messageData)
         case "BalioaTxikitu":
-            decreaseValue()
+            thread_func = Thread(target=decreaseValue, args=(messageData,))
+            # decreaseValue(messageData)
         case "BalioaBiderkatu":
-            multiplyValue()
+            thread_func = Thread(target=multiplyValue, args=(messageData,))
+            # multiplyValue(messageData)
         case _:
             return "Not function selected"
+    # Aukeratu den funtzionalitatea exekuzio-hari berri batean abiarazten dugu
+    thread_func.start()
+    # Aurreko osagaiari erantzuten diogu dena ondo joan dela esanez
+    return "OK"
 
 
 '''
@@ -50,16 +65,87 @@ Funtzionalitateak exekutatzeko metodoak
 '''
 
 
-def increaseValue():
-    pass
+def increaseValue(messageData):
+    # HTTP mezuan bidalitako datuetaki balioa lortuko dugu
+    jsonData = json.loads(messageData)
+    type = jsonData['type']
+    value = jsonData['value']
+
+    # TODO ezabatu
+    step = 2
+
+    # Funtzionalitatearen eragiketa betetzen dugu
+    value += step
+
+    # Sartutako urratsa mota desberdinekoa izanez, emaitza desberdina izan daiteke, beraz ziurtatuko gara
+    match type:
+        case "natural" | "integer":
+            value = int(value)
+        case "float":
+            value = float(value)
+        case _:
+            pass
+    # Balio berria edukiz, hurrengo osagaiari pasako diogu
+    sendData(type, value)
 
 
-def decreaseValue():
-    pass
+def decreaseValue(messageData):
+    # HTTP mezuan bidalitako datuetaki balioa lortuko dugu
+    jsonData = json.loads(messageData)
+    type = jsonData['type']
+    value = jsonData['value']
+
+    # TODO ezabatu
+    step = 2
+
+    # Funtzionalitatearen eragiketa betetzen dugu
+    value -= step
+
+    # Sartutako urratsa mota desberdinekoa izanez, emaitza desberdina izan daiteke, beraz ziurtatuko gara
+    match type:
+        case "natural" | "integer":
+            value = int(value)
+        case "float":
+            value = float(value)
+        case _:
+            pass
+    # Balio berria edukiz, hurrengo osagaiari pasako diogu
+    sendData(type, value)
 
 
-def multiplyValue():
-    pass
+def multiplyValue(messageData):
+    # HTTP mezuan bidalitako datuetaki balioa lortuko dugu
+    jsonData = json.loads(messageData)
+    type = jsonData['type']
+    value = jsonData['value']
+
+    # TODO ezabatu
+    step = 2
+
+    # Funtzionalitatearen eragiketa betetzen dugu
+    value = value * step
+
+    # Sartutako urratsa mota desberdinekoa izanez, emaitza desberdina izan daiteke, beraz ziurtatuko gara
+    match type:
+        case "natural" | "integer":
+            value = int(value)
+        case "float":
+            value = float(value)
+        case _:
+            pass
+    # Balio berria edukiz, hurrengo osagaiari pasako diogu
+    sendData(type, value)
+
+
+def sendData(type, value):
+
+    url = 'http://localhost:8500'
+    headers = {'Content-Type': 'text/plain'}
+    try:
+        r = requests.post(url, headers=headers, data='{"type": "' + type + '", "value": ' + value + '}')
+    except (requests.exceptions.RequestException, requests.exceptions.ConnectionError) as e:
+        print("ERROREA!!! Ezin izan da mezua bidali")
+        print(e)
 
 
 if __name__ == '__main__':
